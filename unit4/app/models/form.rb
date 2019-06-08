515492cc97b448b623844f46eed692b2cdc26ca8
Lauren Mellon <laumellon@gmail.com>
2019-06-08 10:37:38 -0400

    set up db locally

--- unit4/app/models/form.rb
+++ unit4/app/models/form.rb
@@ -4,31 +4,33 @@ class Form
 
   # get all form
   def self.all
-    results = DB.exec("SELECT * FROM form;")
+    results = DB.exec("SELECT * FROM entries;")
     return results.map do |result|
       {
         "id" => result["id"].to_i,
-        "date" => result["date"].to_i,
+        "date" => result["date"],
         "text" => result["text"],
         "image" => result["image"],
         "title" => result["title"],
-        "author" => result["author"]
+        "author" => result["author"],
+        "status" => result["status"]
         }
     end
   end
 
   # get one form
   def self.find(id)
-    results = DB.exec("SELECT * FROM form WHERE id=#{id}")
+    results = DB.exec("SELECT * FROM entries WHERE id=#{id};")
     return {
-     
+
         "id" => results.first["id"].to_i,
-        "date" => results.first["date"].to_i,
+        "date" => results.first["date"],
         "text" => results.first["text"],
         "image" => results.first["image"],
         "title" => results.first["title"],
-        "author" => results.first["author"]
-      
+        "author" => results.first["author"],
+        "status" => results.first["status"]
+
     }
   end
 
@@ -36,24 +38,25 @@ class Form
   def self.create(opts)
     results = DB.exec(
       <<-SQL
-        INSERT INTO form (date, text, image, title, author)
-        VALUES ( #{opts["date"]}, '#{opts["text"]}', '#{opts["image"]}', '#{opts["title"]}', '#{opts["author"]}' )
-        RETURNING id, date, text, image, title, author;
+        INSERT INTO entries (date, text, image, title, author, status)
+        VALUES ( #{opts["date"]}, '#{opts["text"]}', '#{opts["image"]}', '#{opts["title"]}', '#{opts["author"]}', '#{opts["status"]}' )
+        RETURNING id, date, text, image, title, author, status;
       SQL
     )
     return {
-      "id" => results.first["id"].to_i,
-      "date" => results.first["date"].to_i,
+      "id" => results.first["id"],
+      "date" => results.first["date"],
       "text" => results.first["text"],
       "image" => results.first["image"],
       "title" => results.first["title"],
-      "author" => results.first["author"]
+      "author" => results.first["author"],
+      "status" => results.first["status"]
     }
   end
 
   # delete one form
   def self.delete(id)
-    results = DB.exec("DELETE FROM form WHERE id=#{id};")
+    results = DB.exec("DELETE FROM entries WHERE id=#{id};")
     return { "deleted" => true }
   end
 
@@ -61,19 +64,20 @@ class Form
   def self.update id, opts
     results = DB.exec(
       <<-SQL
-        UPDATE form
-        SET date=#{opts["date"]} text='#{opts["text"]}', image='#{opts["image"]}', title='#{opts["title"]}', author='#{opts["author"]}'
+        UPDATE entries
+        SET date=#{opts["date"]} text='#{opts["text"]}', image='#{opts["image"]}', title='#{opts["title"]}', author='#{opts["author"]}', status='#{opts["status"]}'
         WHERE id=#{id}
-        RETURNING id, date, text, image, title, author;
+        RETURNING id, date, text, image, title, author, status;
       SQL
     )
     return {
       "id" => results.first["id"].to_i,
-      "date" => results.first["date"].to_i,
+      "date" => results.first["date"],
       "text" => results.first["text"],
       "image" => results.first["image"],
       "title" => results.first["title"],
-      "author" => results.first["author"]
+      "author" => results.first["author"],
+      "status"=> results.first["status"]
     }
   end
 
