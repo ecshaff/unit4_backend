c4bebe3d9980b9d58135eaaa6c724a0c79da8088
ecshaff <emmyshaff@gmail.com>
2019-06-08 11:38:36 -0700

    models file updated

--- unit4/app/models/form.rb
+++ unit4/app/models/form.rb
@@ -1,7 +1,13 @@
 class Form
   # connecting to database
+  if(ENV['DATABASE_URL'])
+        uri = URI.parse(ENV['DATABASE_URL'])
+        DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+ 
+  else
   DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'unit4_development'})
 
+end
   # get all form
   def self.all
     results = DB.exec("SELECT * FROM form;")
